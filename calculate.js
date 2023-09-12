a0a7b3747484e879bf0e34282bc67fd90f625848
M153 <wanchatgame@gmail.com>
2023-09-13 00:50:08 +0700

    random

--- calculate.js
+++ calculate.js
@@ -1,22 +1,33 @@
-//let Lotto = prompt("Number")
+let Lotto = prompt("Number")
+let randomNumber = Math.floor(Math.random() * 100)
+document.getElementById("random").innerHTML = randomNumber
+
+if(Lotto == randomNumber)
+{
+    document.getElementById("buy").innerHTML = "คุณซื้อหมายเลข: " + Lotto + "ยินดีด้วยคุณถูกรางวัลล!!!"
+}
+else
+{
+    document.getElementById("buy").innerHTML = "คุณซื้อหมายเลข: " + Lotto + "  น่าเสียดายจังคุณไม่ถูกรางวัล 555555"
+}
+
 //console.log(Lotto)
 //document.getElementById("buy").innerHTML = Lotto
 
-//let randomNumber = Math.floor(Math.random() * 100)
 //console.log(randomNumber)
 
 //document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
 
-console.log(1 == 1) //true
-console.log(1 == 2) //false
+// console.log(1 == 1) //true
+// console.log(1 == 2) //false
 
-console.log(2 == "2") //true
-console.log(2 === "2") //false
+// console.log(2 == "2") //true
+// console.log(2 === "2") //false
 
-console.log(3 != "3") //false
-console.log(3 !== "3") //true
+// console.log(3 != "3") //false
+// console.log(3 !== "3") //true
 
-console.log(4 > 4) //false
-console.log(4 < 4) //false
-console.log(4 >= 4) //true
-console.log(4 <= 4) //true
+// console.log(4 > 4) //false
+// console.log(4 < 4) //false
+// console.log(4 >= 4) //true
+// console.log(4 <= 4) //true
