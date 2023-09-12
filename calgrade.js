b5676335dc1b8bad11ef982b855e5fba29237036
M153 <wanchatgame@gmail.com>
2023-09-13 01:09:07 +0700

    else if

---
+++ calgrade.js
@@ -0,0 +1,24 @@
+let score = prompt("คะแนนของคุณ : ")
+if(score >= 80)
+{
+    document.getElementById("grade").innerHTML = "คุณได้เกรด A"
+}
+else if(score >= 70)
+{
+    document.getElementById("grade").innerHTML = "คุณได้เกรด B"
+}
+else if(score >= 60)
+{
+    document.getElementById("grade").innerHTML = "คุณได้เกรด C"
+}
+else if(score >= 50)
+{
+    document.getElementById("grade").innerHTML = "คุณได้เกรด D"
+}
+else if(score >= 0)
+{
+    document.getElementById("grade").innerHTML = "คุณได้เกรด F"
+}else
+{
+    document.getElementById("grade").innerHTML = "Error"
+}
