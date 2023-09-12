a8ea455da218b010c84ce3712259946507706fe7
M153 <wanchatgame@gmail.com>
2023-09-13 00:03:57 +0700

    Truth value

--- calculate.js
+++ calculate.js
@@ -1,8 +1,22 @@
-let Lotto = prompt("Number")
+//let Lotto = prompt("Number")
 //console.log(Lotto)
-document.getElementById("buy").innerHTML = Lotto
+//document.getElementById("buy").innerHTML = Lotto
 
 //let randomNumber = Math.floor(Math.random() * 100)
 //console.log(randomNumber)
 
-document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+//document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+
+console.log(1 == 1) //true
+console.log(1 == 2) //false
+
+console.log(2 == "2") //true
+console.log(2 === "2") //false
+
+console.log(3 != "3") //false
+console.log(3 !== "3") //true
+
+console.log(4 > 4) //false
+console.log(4 < 4) //false
+console.log(4 >= 4) //true
+console.log(4 <= 4) //true
