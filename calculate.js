0240c191505dcd2da3068e90114e561441ed47aa
M153 <wanchatgame@gmail.com>
2023-09-12 23:51:02 +0700

    set get ID

---
+++ calculate.js
@@ -0,0 +1,8 @@
+let Lotto = prompt("Number")
+//console.log(Lotto)
+document.getElementById("buy").innerHTML = Lotto
+
+//let randomNumber = Math.floor(Math.random() * 100)
+//console.log(randomNumber)
+
+document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
