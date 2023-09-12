105548e63f8bfa89b50069225756b308b32a2254
M153 <wanchatgame@gmail.com>
2023-09-12 23:08:22 +0700

    let

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Hello" + " World")
+//alert("Hi")
+
+var name = "Wanchat"
+console.log(name)
+name = "M"
+console.log(name)
+
+//ลำดับตวามสำคัญ ^ยกกำลัง, *คูณ /หาร +บวก -ลบ
+var i = 15 + 20
+console.log(i / 2)
+
+//เช็คประเภทข้อมูล
+console.log(typeof 12)
+
+{
+    var x = 12
+
+    //let จำทำงานแค่ใน Scope เท่านั้น
+    let y = 10
+}
+console.log(x+y)
