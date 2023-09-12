50463084f0b08f62833a41eaf2a38cf16c2e883b
M153 <wanchatgame@gmail.com>
2023-09-13 03:25:41 +0700

    Loop While

--- js/calfood.js
+++ js/calfood.js
@@ -1,9 +1,14 @@
-let qrt = prompt("จำนวนสินค้า")
-let sum = 0
-for(var i = 1; i <= qrt; i++)
+// let qrt = prompt("จำนวนสินค้า")
+// let sum = 0
+// for(var i = 1; i <= qrt; i++)
+// {
+//     let item_price = prompt("ราคาสินค้าชิ้นที่ " + i)
+//     sum = sum + parseInt(item_price)
+//     document.getElementById("price-list").innerHTML += "รายการสินค้าชิ้นที่" + i + ":" + item_price +"บาท"+"<br>"
+// }
+// document.getElementById("result").innerHTML = "ราคารวม " + sum + "บาท"
+
+while(prompt("ถ้าจะออกพิมพ์ Q") != "Q")
 {
-    let item_price = prompt("ราคาสินค้าชิ้นที่ " + i)
-    sum = sum + parseInt(item_price)
-    document.getElementById("price-list").innerHTML += "รายการสินค้าชิ้นที่" + i + ":" + item_price +"บาท"+"<br>"
+    document.getElementById("result").innerHTML += "ไม่ได้พิมพ์ Q <br>"
 }
-document.getElementById("result").innerHTML = "ราคารวม " + sum + "บาท"
